d7bc699ce2302448eabd60f2bef3045818e5ac62
Rishi Javia <rishi.javia.3895@gmail.com>
2016-11-18 11:17:04 -0800

    initializing

---
+++ Interpreter.java
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 11/18/16.
+ * Author: Rishi Javia
+ */
+public class Interpreter {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
