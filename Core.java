cebfc3a7853ee16b6f9b989fdd9deb6590a6e838
Rishi Javia <rishi.javia.3895@gmail.com>
2016-11-18 11:32:17 -0800

    renamed file

--- Core.java
+++ Core.java
@@ -2,7 +2,7 @@
  * Created by root on 11/18/16.
  * Author: Rishi Javia
  */
-public class Interpreter {
+public class Core {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
